a5b0b227d427801a420d76fd1b24525ea74c44e6
dave <david.spak478@gmail.com>
2020-05-18 15:09:12 +0000

    env - api url

--- src/main.js
+++ src/main.js
@@ -3,13 +3,10 @@ import VueRouter from 'vue-router'
 import router from './router'
 import axios from 'axios'
 
-// axios.defaults.baseURL = process.env.VUE_APP_ROOT_API
-// axios.defaults.baseURL = 'http://127.0.0.1/api'
-axios.defaults.baseURL = 'http://wt45.fei.stuba.sk:8045/final-api/api'
+axios.defaults.baseURL = process.env.VUE_APP_ROOT_API
 
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
-
 
 import Master from './views/Master'
 import i18n from './i18n'
